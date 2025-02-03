7951b9a2be263568f00008caf2cce70b55636ac9
nshathish <470487+nshathish@users.noreply.github.com>
2025-02-03 05:49:41 +0000

    new fields added to job

--- Infrastructure/Entities/Job.cs
+++ Infrastructure/Entities/Job.cs
@@ -6,6 +6,8 @@ public class Job
 {
     public int Id { get; set; }
     [MaxLength(500)] public string JobName { get; set; } = null!;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
 
     public int OrderId { get; set; }
 
