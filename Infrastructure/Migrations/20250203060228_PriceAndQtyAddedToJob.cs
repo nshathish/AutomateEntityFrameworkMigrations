59545d7c7f9031979fef7790d58611a5fca2d1ff
nshathish <470487+nshathish@users.noreply.github.com>
2025-02-03 06:19:00 +0000

    print the contents of the migrations.sql

---
+++ Infrastructure/Migrations/20250203060228_PriceAndQtyAddedToJob.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceAndQtyAddedToJob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderName",
+                table: "Orders",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDescription",
+                table: "Orders",
+                type: "nvarchar(3000)",
+                maxLength: 3000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobName",
+                table: "Jobs",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Jobs",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Jobs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Jobs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderDescription",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3000)",
+                oldMaxLength: 3000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobName",
+                table: "Jobs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+        }
+    }
+}
